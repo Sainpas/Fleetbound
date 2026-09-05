3775bd7b0a87dd387bcd7c26d0d674828d785876
Sainpas <pvihervaara@gmail.com>
2026-09-05 21:02:29 +0300

    Add initial Fleetbound ship roster

---
+++ js/ships.js
@@ -0,0 +1,12 @@
+export const ships = [
+  { id: 'hms-nelson', name: 'HMS Nelson', nation: 'HMS', class: 'Nelson', type: 'Battleship', hints: ['Nelson-class battleship'] },
+  { id: 'hms-hood', name: 'HMS Hood', nation: 'HMS', class: 'Admiral-class', type: 'Battlecruiser', hints: ['Admiral-class battlecruiser'] },
+  { id: 'hms-king-george-v', name: 'HMS King George V', nation: 'HMS', class: 'King George V', type: 'Battleship', hints: ['King George V-class battleship'] },
+  { id: 'hms-prince-of-wales', name: 'HMS Prince of Wales', nation: 'HMS', class: 'King George V', type: 'Battleship', hints: ['King George V-class battleship'] },
+  { id: 'hms-belfast', name: 'HMS Belfast', nation: 'HMS', class: 'Town', type: 'Light Cruiser', hints: ['Town-class light cruiser'] },
+  { id: 'kms-bismarck', name: 'Bismarck', nation: 'KMS', class: 'Bismarck', type: 'Battleship', hints: ['Bismarck-class battleship'] },
+  { id: 'kms-tirpitz', name: 'Tirpitz', nation: 'KMS', class: 'Bismarck', type: 'Battleship', hints: ['Bismarck-class battleship'] },
+  { id: 'kms-scharnhorst', name: 'Scharnhorst', nation: 'KMS', class: 'Scharnhorst', type: 'Battleship', hints: ['Scharnhorst-class capital ship'] },
+  { id: 'kms-prinz-eugen', name: 'Prinz Eugen', nation: 'KMS', class: 'Admiral Hipper', type: 'Heavy Cruiser', hints: ['Admiral Hipper-class heavy cruiser'] },
+  { id: 'kms-admiral-hipper', name: 'Admiral Hipper', nation: 'KMS', class: 'Admiral Hipper', type: 'Heavy Cruiser', hints: ['Admiral Hipper-class heavy cruiser'] }
+];
